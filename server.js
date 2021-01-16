32f5a6833c45c0a073878ab5327e2730ef16a5e1
shaggy <simmons5family@gmail.com>
2021-01-16 14:19:17 -0600

    set up routes to serve on server, test User routes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+const express = require('express');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const express = require('express')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(routes);
 
     // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {

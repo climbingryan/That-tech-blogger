596181cfd6d2a5fb50ccc277f3af3290bd3bdc2f
shaggy <simmons5family@gmail.com>
2021-01-19 17:29:01 -0600

    Write partial to format Date on Posts. Working on showing & hiding nav links

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');

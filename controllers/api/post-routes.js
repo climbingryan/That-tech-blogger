85746ae60fbd631ea983d0fba19c0f86b6378d74
shaggy <simmons5family@gmail.com>
2021-01-19 18:49:14 -0600

    add-post logic. Tested and working. Needs CSS overhaul

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -70,9 +70,9 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
     Post.create({
         title: req.body.title,
-        post_content: req.body.post_content,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        post_content: req.body.post_content,
+        user_id: req.session.user_id
     }).then(dbPostData => res.json(dbPostData))
       .catch(err => {
           console.log(err);
